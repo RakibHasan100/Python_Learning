7c80de232b2642c849cee8582bc6b351b70f982a
RakibHasan100 <123709768+RakibHasan100@users.noreply.github.com>
2025-09-06 22:04:19 +0600

    Add files via upload

---
+++ Chapter1_Print_Comment_Package.py
@@ -0,0 +1,52 @@
+#Hello World
+#print("Hello World")
+#print('Hello World')
+
+#module is the code written by other and we can install it using pip(package manager for python)
+'''
+import pyjokes
+foke= pyjokes.get_joke()
+print(foke)
+'''
+
+"""
+import pyjokes
+foke= pyjokes.get_joke()
+print(foke)
+Ctrl+/
+"""
+
+#REPL read evaluate print loop... python...open command prompt,type python and now it can calculate and also python code can be run
+
+
+
+#Multi-line print
+'''
+print("""HELLO
+WORLD
+HOW
+ARE
+YOU?""")
+'''
+
+"""
+print('''HELLO
+WORLD
+HOW
+ARE
+YOU?''')
+"""
+
+'''import pyttsx3
+engine = pyttsx3.init()
+engine.say("Anaconda")
+engine.runAndWait()'''
+
+
+'''
+import os
+dr="F:\Office 2024";#it will also work without semicolon
+contents=os.listdir(dr)
+for i in contents:
+    print(i)
+'''
---
+++ Chapter2_Operators.py
@@ -0,0 +1,110 @@
+'''
+a = 1;#a is an integer
+b = 2
+print(a+b);#without (;) also work
+
+c='rakib'#double quotation also work#c is a string
+print(c)
+d=3.3838378#d is a float
+print(d)
+'''
+#e = False#e is a boolean variabvle
+#f = None#f is a none type variable
+#a=3
+#aaa=4
+#rakib=5
+#_rakib=6
+#@rakib=7#cannot use @ as a variable
+#ra@kib=7#cannot use @ as a variable
+#variable name can contain alphabets,digits and ubderscores
+#can start with alphabets and underscores b ut not with digits
+#no white space between inside a variable name is allowed
+##############46:11#########################################
+
+#Arithmetic Operator
+#a = 7
+#b = 4
+#c = a + b
+#print(a+b)
+#print(c)
+
+
+#Assignment Operator
+'''
+a = 7
+b = 8
+print(a)
+print(b)
+a += 2
+b -= 2
+print(a)
+print(b)
+a *= 2
+b /= 2
+print(a)
+print(b)
+'''
+
+#Comparison Operator
+'''
+a = 3
+b = 4
+c = 3
+d = b > c
+print(d)
+print(a == b)
+print(a == c)
+print(a > b)
+print(a < b)
+print(a >= c)
+print(a <= c)
+print(a != c)
+'''
+#Logical Operator
+'''
+e = True or False#Think True as 1 and False as 2
+f = True and False
+print(f)
+print(not(True))
+print(not(False))
+'''
+
+#Type and Typecasting
+'''
+a = 1
+b = 1.2
+c = 'A.Rakib'
+d = "A.Rakib"
+print(type(d))
+e = "3.2"
+f = float(e)
+g = 6.7
+h = int(g)
+print(f,type(f))
+print(h,type(h))
+print(d,type(d))#Undefined
+'''
+
+#Input Function
+'''
+a = input('1:')#3, take input as string
+#a = int(input('1:'))#3, take input as int
+b = input("2:")#4, take input as string
+print(a+b)#It will print 34
+print(int(a)+int(b))#It will print 7
+print('Md.' + " Rakib" + ' ' + "Hasan")# Minus won't work and if we open ',should close ', same for " and "
+'''
+
+#Practice of chapter 2
+'''
+print('Remainder of 3 divided by 2 = ',3%2)
+print('square and cube of 3 = ' , 3*3 , 'or' , 3**2 , "and" , 3**3)
+print('square of 3 = ' , 3^2)#Incorrect
+'''
+
+
+
+
+
+
+
